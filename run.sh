d1e2f8af6301d6f365919d665f9120fdd10803e9
liam-b <liam@anywhich.com>
2019-03-18 13:51:30 +1100

    removed .DS_Store

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 [ -z "$1" ] && echo "no remote supplied" && exit 1
-
 env GOOS=linux GOARCH=arm GOARM=5 go build
 scp ./robocup-2019 $1:/home/robot/src/bin
 ssh -t $1 "/home/robot/src/bin/robocup-2019"
