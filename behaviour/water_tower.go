b1445914270e7058cd4b833cbcc695fb177cfb4f
Your Name <you@example.com>
2019-06-30 12:16:41 +1000

    PastaLaVist

--- behaviour/water_tower.go
+++ behaviour/water_tower.go
@@ -2,36 +2,50 @@ package behaviour
 
 import (
 	"github.com/liam-b/robocup-2019/bot"
-	// "github.com/liam-b/robocup-2019/helper"
+	"github.com/liam-b/robocup-2019/helper"
 	"github.com/liam-b/robocup-2019/state_machine"
-  // "github.com/liam-b/robocup-2019/logger"
+  "github.com/liam-b/robocup-2019/logger"
 )
 
 
-var waterTower = Behaviour{
+var water_tower = Behaviour{
   Setup: func() {
     state_machine.Add(state_machine.State{
       Name: "water_tower.verify",
-      Enter: func() {
-        bot.DriveMotorLeft.RunToRelativePositionAndCoast(200, 100)
-        bot.DriveMotorRight.RunToRelativePositionAndCoast(200, 100)
-      },
       Update: func() {
-        // if (bot.UltrasonicSensor.Distance() <= 20000) {
-        //   state_machine.Transition("waterTower.skrrrt")
-        // }
-        // logger.Trace(bot.UltrasonicSensor.Distance())
+        if (bot.UltrasonicSensor.Distance() <= 4000) {
+          // helper.CloseClaw()
+          state_machine.Transition("water_tower.adjust")
+        } 
+        logger.Trace(bot.UltrasonicSensor.Distance())
       },
     })
 
     state_machine.Add(state_machine.State{
-      Name: "waterTower.skrrrt",
+      Name: "water_tower.adjust",
+      Enter: func() {
+        helper.CloseClaw()
+        bot.DriveMotorRight.RunToRelativePositionAndHold(-150, 300)
+        bot.DriveMotorLeft.RunToRelativePositionAndHold(150, 300)        
+      },
       Update: func() {
-        // bot.DriveMotorRight.ResetPosition()
-        // bot.DriveMotorRight.Run()
-
+        if (helper.IsDriveHolding()) {
+          state_machine.Transition("water_tower.skrrrt")
+        }
       },
     })
 
+    state_machine.Add(state_machine.State{
+      Name: "water_tower.skrrrt",
+      Update: func() {
+        if (bot.ColorSensorMiddle.Intensity() >= 10) { // the problem is that it is starting on black, changed to middle sensor try again
+          bot.DriveMotorLeft.Run(200)
+          bot.DriveMotorRight.Run(290)
+        } else {
+          state_machine.Transition("follow_line.follow")
+        }
+      },
+    })
   },
 }
+
